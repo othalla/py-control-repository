d53576341ade1c19efdf84702bf9b9e1a7a7bee5
othalla <othalla.lf@gmail.com>
2018-06-13 08:34:44 +0200

    [Add] control_repository module

---
+++ ppfilemgr/control_repository.py
@@ -0,0 +1,44 @@
+# from github import Github
+from ppfilemgr.puppet_module import PuppetModule
+from ppfilemgr.parser import parse_puppetfile
+from ppfilemgr.github import update_puppetfile, get_puppetfile
+
+
+class ControlRepository:
+    def __init__(self, environment, github_orga, github_repo, github_token,
+                 github_baseurl=None, puppetfile_name='Puppetfile'):
+        self._environment = environment
+        self._github_orga = github_orga
+        self._github_repo = github_repo
+        self._github_token = github_token
+        self._github_baseurl = github_baseurl
+        self._puppetfile_name = puppetfile_name
+        self._puppetfile = None
+
+    @property
+    def modules(self):
+        if self._puppetfile is not None:
+            return self._puppetfile.modules
+        return []
+
+    def parse(self, file_provider=get_puppetfile,
+              parser_provider=parse_puppetfile):
+        if self._puppetfile is None:
+            current_puppetfile = file_provider(self._environment,
+                                               self._github_orga,
+                                               self._github_repo,
+                                               self._github_token,
+                                               self._github_baseurl,
+                                               self._puppetfile_name)
+            self._puppetfile = parser_provider(current_puppetfile)
+
+    def update(self, puppet_module, update_puppetfile_provider=update_puppetfile):
+        if not isinstance(puppet_module, PuppetModule):
+            raise TypeError
+        for module in self._puppetfile.modules:
+            if module.name == puppet_module.name:
+                if not module != puppet_module:  # updaten module ref changed
+                    return
+                self._puppetfile.update_module(puppet_module)
+        self._puppetfile.add_module(puppet_module)
+        return update_puppetfile_provider(self._puppetfile)
---
+++ tests/test_control_repository.py
@@ -0,0 +1,68 @@
+from unittest.mock import Mock
+import pytest
+
+from github.GithubException import GithubException
+
+from ppfilemgr.control_repository import ControlRepository
+from ppfilemgr.puppet_module import ForgeModule
+from ppfilemgr.puppetfile import Puppetfile
+
+
+class TestControlRepository:
+    def test_parse_calls_right_functions_without_errors(self):
+        file_content = Mock(name='file_content')
+        file_content.return_value = ['line1', 'line2']
+        parse_puppetfile = Mock(name='parse_puppetfile')
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        control_repo.parse(file_provider=file_content, parser_provider=parse_puppetfile)
+        file_content.assert_called_once_with('env', 'orga', 'repo', 'token', None, 'Puppetfile')
+        parse_puppetfile.assert_called_once_with(['line1', 'line2'])
+
+    def test_parse_raise_github_exception_if_file_content_fails(self):
+        file_content = Mock(name='file_content')
+        file_content.side_effect = GithubException('status', 'data')
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        with pytest.raises(GithubException):
+            control_repo.parse(file_provider=file_content)
+
+    def test_update_raise_type_error_with_bad_input_module(self):
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        with pytest.raises(TypeError):
+            control_repo.update('badmodule')
+
+    def test_update_add_new_module_to_puppetfile(self):
+        file_content = Mock(name='file_content')
+        parse_puppetfile = Mock(name='parse_puppetfile')
+        update_puppetfile = Mock(name='update_puppetfile')
+        parse_puppetfile.return_value = Puppetfile()
+        forge_module = ForgeModule('nginx', '0.0.1')
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        control_repo.parse(file_provider=file_content, parser_provider=parse_puppetfile)
+        control_repo.update(forge_module, update_puppetfile_provider=update_puppetfile)
+        assert forge_module in control_repo.modules
+        assert update_puppetfile.call_count == 1
+
+    def test_update_existing_module_does_nothing_with_same_version(self):
+        forge_module = ForgeModule('nginx', '0.0.1')
+        file_content = Mock(name='file_content')
+        update_puppetfile = Mock(name='update_puppetfile')
+        parse_puppetfile = Mock(name='parse_puppetfile')
+        parse_puppetfile.return_value.modules = [forge_module]
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        control_repo.parse(file_provider=file_content, parser_provider=parse_puppetfile)
+        control_repo.update(forge_module, update_puppetfile_provider=update_puppetfile)
+        assert update_puppetfile.call_count == 0
+
+    def test_update_existing_module_does_change_puppetfile(self):
+        file_content = Mock(name='file_content')
+        update_puppetfile = Mock(name='update_puppetfile')
+        parse_puppetfile = Mock(name='parse_puppetfile')
+        ppfile = Puppetfile()
+        ppfile.add_module(ForgeModule('nginx', '0.0.1'))
+        parse_puppetfile.return_value = ppfile
+        control_repo = ControlRepository('env', 'orga', 'repo', 'token')
+        control_repo.parse(file_provider=file_content, parser_provider=parse_puppetfile)
+        forge_module = ForgeModule('nginx', '0.0.2')
+        control_repo.update(forge_module, update_puppetfile_provider=update_puppetfile)
+        assert forge_module == control_repo.modules[0]
+        assert update_puppetfile.call_count == 1
