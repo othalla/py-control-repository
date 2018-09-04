9be63989a1943bde4db14b250c44eb4c1de3558b
othalla <othalla.lf@gmail.com>
2018-09-04 07:28:57 +0200

    improve naming in puppet module tests

--- tests/test_puppet_module.py
+++ tests/test_puppet_module.py
@@ -5,7 +5,7 @@ from ppfilemgr.puppet_module import GitModule
 
 
 class TestGitModule:
-    def test_it_raise_bad_git_reference_type_with_unknow_reference(self):
+    def test_with_unknow_reference(self):
         with pytest.raises(ModuleBadGitReferenceTypeExcption):
-            GitModule('nginx', 'https://url/repo/nginx.git', 'bad_ref',
+            GitModule('nginx', 'https://url/repo/nginx.git', 'bad_reference',
                       'version')
