42ef728e2b204a6f869af3b2cd6bd3fea94f3793
othalla <othalla.lf@gmail.com>
2018-09-08 23:58:23 +0200

    fix control repository

--- control_repository/control_repository.py
+++ control_repository/control_repository.py
@@ -3,14 +3,11 @@ from github import Github
 
 class ControlRepository:
     def __init__(self, github_orga: str, github_repo: str,
-                 github_token: str, github_baseurl=None,
-                 puppetfile_name='Puppetfile') -> None:
+                 github_token: str, github_baseurl=None) -> None:
         self._github_orga = github_orga
         self._github_repo = github_repo
         self._github_token = github_token
         self._github_baseurl = github_baseurl
-
-/bin/bash: :w: command not found
 
     def get_environment(self):
         pass
