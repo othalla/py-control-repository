24c75ec35f39adb5f29cb995d649d61e2388b02d
othalla <othalla.lf@gmail.com>
2018-09-09 00:20:57 +0200

    control repository get environment returns an environment

--- control_repository/control_repository.py
+++ control_repository/control_repository.py
@@ -1,13 +1,17 @@
 from github import Github
 
+from control_repository.puppet import Environment
+
 
 class ControlRepository:
-    def __init__(self, github_organization: str, github_repository: str,
-                 github_token: str, github_baseurl=None) -> None:
+    def __init__(self, github_organization: str,
+                 github_repository: str,
+                 github_token: str,
+                 github_baseurl=None) -> None:
         self._github_organization = github_organization
         self._github_repository = github_repository
         self._github_token = github_token
         self._github_baseurl = github_baseurl
 
-    def get_environment(self):
-        pass
+    def get_environment(self, environment):
+        return Environment()
