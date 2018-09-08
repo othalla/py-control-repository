e5f591d0b1175236ba290879f3c384daa419ec8b
othalla <othalla.lf@gmail.com>
2018-09-09 00:00:24 +0200

    improve variable naming for control repository

--- control_repository/control_repository.py
+++ control_repository/control_repository.py
@@ -2,10 +2,10 @@ from github import Github
 
 
 class ControlRepository:
-    def __init__(self, github_orga: str, github_repo: str,
+    def __init__(self, github_organization: str, github_repository: str,
                  github_token: str, github_baseurl=None) -> None:
-        self._github_orga = github_orga
-        self._github_repo = github_repo
+        self._github_organization = github_organization
+        self._github_repository = github_repository
         self._github_token = github_token
         self._github_baseurl = github_baseurl
 
