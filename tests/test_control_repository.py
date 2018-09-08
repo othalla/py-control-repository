849fb57a21a5686b5a0dfea46ff8713f81a7aef9
othalla <othalla.lf@gmail.com>
2018-09-09 00:20:37 +0200

    improve test for control repository get environment

--- tests/test_control_repository.py
+++ tests/test_control_repository.py
@@ -4,6 +4,7 @@ from github import GithubException
 import pytest
 
 from control_repository.control_repository import ControlRepository
+from control_repository.puppet import Environment
 
 
 class TestControlRepositoryGetEnvironment:
@@ -12,3 +13,9 @@ class TestControlRepositoryGetEnvironment:
         control_repository = ControlRepository('test_organization',
                                                'test_repository',
                                                'some-token')
+        puppet_environment = control_repository.get_environment('test')
+        assert isinstance(puppet_environment, Environment)
+
+    @staticmethod
+    def test_if_environment_does_not_exists():
+        pass
