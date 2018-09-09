13e242c17ce1a753c747516c483e167dc8beae33
othalla <othalla.lf@gmail.com>
2018-09-09 17:33:04 -0400

    add get_puppetfile to Environment class

--- control_repository/puppet/environment.py
+++ control_repository/puppet/environment.py
@@ -1,6 +1,11 @@
-from github import Repository
+from github.Repository import Repository
+
+from control_repository.puppet.puppetfile import Puppetfile
 
 class Environment:
     def __init__(self, name: str, github_repository: Repository) -> None:
         self._name = name
         self._github_repository = github_repository
+
+    def get_puppetfile(self) -> Puppetfile:
+        return Puppetfile()
--- tests/puppet/test_environment.py
+++ tests/puppet/test_environment.py
@@ -1,2 +1,12 @@
-class TestEnvironment:
-    pass
+from unittest.mock import MagicMock
+
+from control_repository.puppet.environment import Environment
+from control_repository.puppet.puppetfile import Puppetfile
+
+class TestEnvironmentGetPuppetfile:
+    @staticmethod
+    def test_it_returns_a_puppetfile():
+        github_repository = MagicMock()
+        environment = Environment('production', github_repository)
+        puppetfile = environment.get_puppetfile()
+        assert isinstance(puppetfile, Puppetfile)
