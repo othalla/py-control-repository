810132356d681af8c3c01a64c1701d1e9b25d212
othalla <othalla.lf@gmail.com>
2018-09-10 16:22:47 -0400

    enhance typing in control_repository module

--- control_repository/control_repository.py
+++ control_repository/control_repository.py
@@ -12,12 +12,12 @@ class ControlRepository:
     def __init__(self, github_organization: str,
                  github_repository_name: str,
                  github_token: str,
-                 github_baseurl: Optional[int] = None) -> None:
-        self._github_organization = github_organization
-        self._github_repository_name = github_repository_name
-        self._github_token = github_token
-        self._github_baseurl = github_baseurl
-        self._github_repository = self._get_github_repository()
+                 github_baseurl: Optional[str] = None) -> None:
+        self._github_organization: str = github_organization
+        self._github_repository_name: str = github_repository_name
+        self._github_token: str = github_token
+        self._github_baseurl: Optional[str] = github_baseurl
+        self._github_repository: Repository = self._get_github_repository()
 
     def get_environment(self, environment: str) -> Environment:
         try:
