78a964aff435ad195383d822eead231485a76a41
othalla <othalla.lf@gmail.com>
2018-09-10 16:14:11 -0400

    Puppetfile first version with empty content

--- control_repository/puppet/puppetfile.py
+++ control_repository/puppet/puppetfile.py
@@ -1,3 +1,26 @@
+from typing import List
+
+from github.Repository import Repository
+
+from control_repository.puppet_module import ForgeModule, GitModule
+
+
 class Puppetfile:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, content: str, github_repository: Repository) -> None:
+        self._content: str = content
+        self._github_repository: Repository = github_repository
+        self._forge_modules: List[ForgeModule] = []
+        self._git_modules: List[GitModule] = []
+        self._forge_url: str = ''
+
+    @property
+    def forge_modules(self) -> ForgeModule:
+        return self._forge_modules
+
+    @property
+    def git_modules(self) -> GitModule:
+        return self._git_modules
+
+    @property
+    def forge_url(self) -> str:
+        return self._forge_url
---
+++ tests/puppet/test_puppetfile.py
@@ -0,0 +1,13 @@
+from unittest.mock import MagicMock
+
+from control_repository.puppet.puppetfile import Puppetfile
+
+
+class TestPuppetfile:
+    @staticmethod
+    def test_with_an_empty_puppetfile():
+        github_repository = MagicMock()
+        puppetfile = Puppetfile('', github_repository)
+        assert puppetfile.forge_modules == []
+        assert puppetfile.git_modules == []
+        assert puppetfile.forge_url == ''
