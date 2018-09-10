733d2952cb5a76628b826db6271ce6a9bd7f40be
othalla <othalla.lf@gmail.com>
2018-09-10 16:21:11 -0400

    enhance typing in environment module

--- control_repository/puppet/environment.py
+++ control_repository/puppet/environment.py
@@ -7,8 +7,8 @@ from control_repository.puppet.puppetfile import Puppetfile
 
 class Environment:
     def __init__(self, name: str, github_repository: Repository) -> None:
-        self._name = name
-        self._github_repository = github_repository
+        self._name: str = name
+        self._github_repository: Repository = github_repository
 
     def get_puppetfile(self) -> Puppetfile:
         try:
