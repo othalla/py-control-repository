b1c04e51098dd31e5c7bbfeb19996611a28af70a
othalla <othalla.lf@gmail.com>
2018-09-11 18:48:04 +0200

    TestGitModule def to staticethod

--- tests/test_puppet_module.py
+++ tests/test_puppet_module.py
@@ -6,7 +6,8 @@ from control_repository.puppet_module import GitModule, ForgeModule
 
 
 class TestGitModule:
-    def test_with_unknow_reference(self):
+    @staticmethod
+    def test_with_unknow_reference():
         with pytest.raises(ModuleBadGitReferenceTypeExcption):
             GitModule('nginx', 'https://url/repo/nginx.git', 'bad_reference',
                       'version')
