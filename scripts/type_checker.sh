6658d71a522d9d4f01f2a463831e43366ba43a8c
othalla <othalla.lf@gmail.com>
2018-09-29 20:34:07 +0200

    Run mypy in script mode & force typing for defs

--- scripts/type_checker.sh
+++ scripts/type_checker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-find control_repository -name "*.py" | xargs mypy --ignore-missing-imports
+find control_repository -name "*.py" | xargs mypy --ignore-missing-imports --strict --disallow-untyped-defs --disallow-untyped-calls --disallow-incomplete-defs
